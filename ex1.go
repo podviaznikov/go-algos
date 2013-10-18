95be456d9cdf12962db6078b5ccda38fcdfae540
Anton Podviaznikov <podviaznikov@gmail.com>
2013-10-18 14:35:19 -0400

    count number of elements in array

--- ex1.go
+++ ex1.go
@@ -12,8 +12,17 @@ func sum(data []int) int {
   return data[0] + sum(data[1:]) 
 }
 
+
+func count(data []int) int {
+  // don't know how to check if array is empty in golang
+  if len(data) == 0 {
+    return 0
+  }
+  return 1 + count(data[1:])
+}
 func main() {
     data := []int{44, 4, 2, 47, 4, 8, 12, 1, 0, 2, 9, 23}
     fmt.Println("initial:", data)
     fmt.Println("sum:", sum(data))
+    fmt.Println("sum:", count(data))
 }
